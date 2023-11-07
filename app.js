2f71fdf0b7bad3f366b7a326cbc0d50c71dfd657
subhrato <subhratosingh@gmail.com>
2023-11-07 20:45:44 +0530

    new app

--- app.js
+++ app.js
@@ -1,43 +1,148 @@
-const express = require('express')
-const path = require("path");
-const app = express()
+const express = require("express");
+const app = express();
+const mysql = require("mysql2");
+const session = require("express-session");
 
-// #############################################################################
-// Logs all request paths and method
-app.use(function (req, res, next) {
-  res.set('x-timestamp', Date.now())
-  res.set('x-powered-by', 'cyclic.sh')
-  console.log(`[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.path}`);
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+// Configure the express-session middleware
+app.use(
+  session({
+    secret: "xenonstack", // Replace with a secret key for session data encryption
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }, // Set secure to true for HTTPS
+  })
+);
+
+// DB connection
+const connection = mysql.createConnection({
+  host: "103.38.50.111",
+  user: "demo",
+  password: "password",
+  database: "demo",
+  port: 3306,
+});
+connection.connect((err) => {
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  console.log("Connected to the database.");
+});
+// -------------
+
+function isLoggedIn(req, res, next) {
+  // console.log(req.session.user);
+  if (!req.session.user) {
+    return res.redirect("/login");
+  }
   next();
+}
+
+app.post("/api/login", (req, res) => {
+  let { email, password } = req.body;
+  if (!(email && password)) {
+    return res.json({ code: 1, message: "All fields are required." });
+  }
+  let checkUser = `SELECT * FROM users WHERE email='${email}' AND password='${password}'`;
+  connection.query(checkUser, (e, row) => {
+    if (e) {
+      return res.json({ code: 1, message: e.message });
+    }
+
+    if (row.length === 0) {
+      return res.json({ code: 1, message: "Invalid Email or Password" });
+    }
+
+    req.session.user = {
+      email: email,
+      firstName: row[0].firstName,
+      lastName: row[0].lastName,
+    };
+    res.json({ code: 2, message: "User Authenticated Successfully" });
+  });
 });
 
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
-var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-  index: ['index.html'],
-  maxAge: '1m',
-  redirect: false
-}
-app.use(express.static('public', options))
+app.post("/api/register", (req, res) => {
+  let { firstName, lastName, email, password } = req.body;
+  if (!(firstName && lastName && email && password)) {
+    return res.json({ code: 1, message: "All fields are required." });
+  }
+  let alreadyExist = `SELECT * FROM users WHERE email='${email}'`;
+  connection.query(alreadyExist, (e, row) => {
+    if (e) {
+      return res.json({ code: 1, message: e.message });
+    }
 
-// #############################################################################
-// Catch all handler for all other request.
-app.use('*', (req,res) => {
-  res.json({
-      at: new Date().toISOString(),
-      method: req.method,
-      hostname: req.hostname,
-      ip: req.ip,
-      query: req.query,
-      headers: req.headers,
-      cookies: req.cookies,
-      params: req.params
-    })
-    .end()
-})
+    if (row.length > 0) {
+      return res.json({ code: 1, message: "User already exists." });
+    }
 
-module.exports = app
+    let sqlQuery = `INSERT INTO users(firstName, lastName, email, password) VALUES ('${firstName}', '${lastName}', '${email}', '${password}')`;
+    connection.query(sqlQuery, (e) => {
+      if (e) {
+        return res.json({ code: 1, message: e.message });
+      }
+      res.json({ code: 2, message: "Account created successfully" });
+    });
+  });
+});
+
+app.post("/api/contact", (req, res) => {
+  let { email, fullName, message } = req.body;
+  if (!(email && fullName && message)) {
+    return res.json({ code: 1, message: "All fields are required." });
+  }
+  let sqlQuery = `INSERT INTO contact(email, fullName, message) VALUES ('${email}', '${fullName}', '${message}')`;
+  connection.query(sqlQuery, (e) => {
+    if (e) {
+      return res.json({ code: 1, message: e.message });
+    }
+    res.json({ code: 2, message: "Form submitted successfully" });
+  });
+});
+
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log("Error destroying the session: " + err);
+    }
+    res.redirect("/login");
+  });
+});
+
+app.get("/dashboard", isLoggedIn, (req, res) => {
+  let { email, firstName, lastName } = req.session.user;
+  res.render("dashboard", {
+    title: "User Dashboard",
+    email,
+    firstName,
+    lastName,
+  });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login", { title: "Login Page" });
+});
+
+app.get("/register", (req, res) => {
+  res.render("register", { title: "Create New Account" });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { title: "Contact Us" });
+});
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home Page" });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
